167ec6d65067fdd824d3e83ca0c9001258d6266f
Taranchuk <nfhfyxer@gmail.com>
2022-09-07 04:58:05 +0300

    some work saved

--- 1.3/Source/CompVehicleMovementController.cs
+++ 1.3/Source/CompVehicleMovementController.cs
@@ -1,4 +1,6 @@
 ﻿using SmashTools;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Vehicles;
 using Verse;
@@ -14,16 +16,18 @@ namespace VanillaVehiclesExpanded
         }
     }
 
-    public enum MovementMode { Accelerate, MaxSpeed, Decelerate}
+    public enum MovementMode { Starting, Accelerate, CurrentSpeed, Decelerate}
+
     [HotSwappable]
     public class CompVehicleMovementController : VehicleComp
     {
         public VehiclePawn Vehicle => this.parent as VehiclePawn;
         public float currentSpeed;
         public bool wasMoving;
-        private float AccelerationRate => Vehicle.GetStatValue(VVE_DefOf.AccelerationRate);
-
         public MovementMode curMovementMode;
+        public List<float> recordedSpeeds = new List<float>();
+        public float AccelerationRate => Vehicle.GetStatValue(VVE_DefOf.AccelerationRate);
+
         public void StartMove()
         {
             if (wasMoving is false)
@@ -31,6 +35,7 @@ namespace VanillaVehiclesExpanded
                 currentSpeed = 0;
             }
             curMovementMode = MovementMode.Accelerate;
+            recordedSpeeds.Clear();
             Log.Message("Starting move: " + currentSpeed + " - " + wasMoving);
         }
 
@@ -46,33 +51,56 @@ namespace VanillaVehiclesExpanded
                     var firstNodeCost = CostToMoveIntoCell(Vehicle, Vehicle.Position, Vehicle.vPather.curPath.FirstNode);
                     ticksToDestination -= firstNodeCost - Vehicle.vPather.nextCellCostLeft;
                 }
-                var decelerateInTicks = ticksToDestination - (currentSpeed / AccelerationRate);
-                var decelerateMultiplier = ticksToDestination / (currentSpeed / AccelerationRate);
-                if (decelerateInTicks <= 10f && currentSpeed > 0)
+                var averageSpeed = recordedSpeeds.Any() ? recordedSpeeds.Average() : currentSpeed;
+                var decelerateMultiplier = ticksToDestination / (averageSpeed / AccelerationRate);
+                var decelerateInTicks = ticksToDestination - (averageSpeed / (AccelerationRate));
+                //LogMode("Init", ticksToDestination, decelerateMultiplier, decelerateInTicks);
+                if ((decelerateInTicks <= 0f) && currentSpeed > 0)
                 {
+                    Vehicle.Map.debugDrawer.FlashCell(Vehicle.Position, 0.1f, duration: 10000);
                     curMovementMode = MovementMode.Decelerate;
+                    var newSpeed = currentSpeed - (AccelerationRate);
+                    if (newSpeed <= 0.5f && Vehicle.vPather.nextCellCostLeft > 0)
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        currentSpeed = Mathf.Max(0, newSpeed);
+                    }
                     if (decelerateMultiplier <= 0.9f)
                     {
-                        Log.Message("Handbraking");
-                        Vehicle.Map.debugDrawer.FlashCell(Vehicle.Position);
-                        Find.TickManager.CurTimeSpeed = TimeSpeed.Paused;
+                        LogMode("Handbraking", ticksToDestination, decelerateMultiplier, decelerateInTicks);
+                        //Find.TickManager.CurTimeSpeed = TimeSpeed.Paused;
                     }
-                    currentSpeed = Mathf.Max(0, currentSpeed - (AccelerationRate / decelerateMultiplier));
-                    Log.Message("Deceleration: - currentSpeed: " + currentSpeed + " ticks left: " + ticksToDestination + " - decelerateInTicks: " + decelerateInTicks + " - decelerateMultiplier: " + decelerateMultiplier + " - next node cost left: " + Vehicle.vPather.nextCellCostLeft);
+                    else
+                    { 
+                        LogMode("Deceleration", ticksToDestination, decelerateMultiplier, decelerateInTicks); 
+                    }
                 }
-                else if (curMovementMode != MovementMode.Decelerate && GetMoveSpeed() > currentSpeed)
+                else if (curMovementMode != MovementMode.Decelerate && GetMoveSpeed() > currentSpeed && decelerateInTicks > 60)
                 {
                     curMovementMode = MovementMode.Accelerate;
                     currentSpeed = Mathf.Min(currentSpeed + AccelerationRate, GetMoveSpeed());
-                    Log.Message("Accelerate: - currentSpeed: " + currentSpeed + " ticks left: " + ticksToDestination + " - decelerateInTicks: " + decelerateInTicks + " - decelerateMultiplier: " + decelerateMultiplier + " - next node cost left: " + Vehicle.vPather.nextCellCostLeft);
+                    LogMode("Acceleration", ticksToDestination, decelerateMultiplier, decelerateInTicks);
+                    Vehicle.Map.debugDrawer.FlashCell(Vehicle.Position, 0.5f, duration: 10000);
                 }
                 else
                 {
-                    curMovementMode = MovementMode.MaxSpeed;
-                    Log.Message("Max speed: - currentSpeed: " + currentSpeed + " ticks left: " + ticksToDestination + " - decelerateInTicks: " + decelerateInTicks + " - decelerateMultiplier: " + decelerateMultiplier + " - next node cost left: " + Vehicle.vPather.nextCellCostLeft);
+                    Vehicle.Map.debugDrawer.FlashCell(Vehicle.Position, 0.7f, duration: 10000);
+                    LogMode("Cur speed intact", ticksToDestination, decelerateMultiplier, decelerateInTicks);
+                    curMovementMode = MovementMode.CurrentSpeed;
                 }
+                recordedSpeeds.Add(currentSpeed);
             }
+            Log.ResetMessageCount();
         }
+
+        private void LogMode(string logMode, float ticksToDestination, float decelerateMultiplier, float decelerateInTicks)
+        {
+            Log.Message(logMode + ": currentSpeed: " + currentSpeed + " average speed: " + (recordedSpeeds.Any() ? recordedSpeeds.Average() : 0) + " move speed: " + GetMoveSpeed() + " ticks left: " + ticksToDestination + " - decelerateInTicks: " + decelerateInTicks + " - decelerateMultiplier: " + decelerateMultiplier + " - next node cost left: " + Vehicle.vPather.nextCellCostLeft + " - Vehicle.vPather.curPath: " + Vehicle.vPather.curPath);
+        }
+
         public float GetTicksToDestination()
         {
             var cost = 0f;
@@ -101,6 +129,8 @@ namespace VanillaVehiclesExpanded
         private static int CostToMoveIntoCell(VehiclePawn vehicle, IntVec3 prevCell, IntVec3 c)
         {
             int num;
+
+            VehicleStatPart_AccelerationRate.modifyValue = true;
             if (c.x == prevCell.x || c.z == prevCell.z)
             {
                 num = vehicle.TicksPerMoveCardinal;
@@ -109,6 +139,8 @@ namespace VanillaVehiclesExpanded
             {
                 num = vehicle.TicksPerMoveDiagonal;
             }
+            VehicleStatPart_AccelerationRate.modifyValue = false;
+
             num += vehicle.Map.GetCachedMapComponent<VehicleMapping>()[vehicle.VehicleDef].VehiclePathGrid.CalculatedCostAt(c);
             Building edifice = c.GetEdifice(vehicle.Map);
             if (edifice != null)
@@ -165,8 +197,14 @@ namespace VanillaVehiclesExpanded
         public override void PostExposeData()
         {
             base.PostExposeData();
+            Scribe_Values.Look(ref wasMoving, "wasMoving");
             Scribe_Values.Look(ref currentSpeed, "currentSpeed");
             Scribe_Values.Look(ref curMovementMode, "curMovementMode");
+            Scribe_Collections.Look(ref recordedSpeeds, "recordedSpeeds", LookMode.Value);
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                recordedSpeeds ??= new List<float>();
+            }
         }
     }
 }
---
+++ 1.3/Source/VehicleStatPart_AccelerationRate.cs
@@ -0,0 +1,31 @@
+﻿using Vehicles;
+using Verse;
+
+namespace VanillaVehiclesExpanded
+{
+    [HotSwappable]
+    public class VehicleStatPart_AccelerationRate : VehicleStatPart
+    {
+        public static bool modifyValue;
+        public override bool Disabled(VehiclePawn vehicle)
+        {
+            return base.Disabled(vehicle) && vehicle.GetComp<CompVehicleMovementController>() is null;
+        }
+        public override string ExplanationPart(VehiclePawn vehicle)
+        {
+            var comp = vehicle.GetComp<CompVehicleMovementController>();
+            return "VVE_StatsReport_AccelerationRate".Translate(comp.AccelerationRate, comp.currentSpeed);
+        }
+
+        public override float TransformValue(VehiclePawn vehicle, float value)
+        {
+            if (modifyValue)
+            {
+                var comp = vehicle.GetComp<CompVehicleMovementController>();
+                //Log.Message("Returning " + comp.currentSpeed + " instead of " + value);
+                return comp.currentSpeed;
+            }
+            return value;
+        }
+    }
+}
