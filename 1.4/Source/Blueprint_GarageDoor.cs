53099753a38d8db9353426b042f0d7779613a216
Taranchuk <nfhfyxer@gmail.com>
2022-10-29 10:54:58 +0300

    blueprint fix

---
+++ 1.4/Source/Blueprint_GarageDoor.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaVehiclesExpanded
+{
+    public class Blueprint_GarageDoor : Blueprint_Build
+    {
+        public override Graphic Graphic
+        {
+            get
+            {
+                var graphic = base.Graphic;
+                var thingDef = this.def.entityDefToBuild as ThingDef;
+                graphic.data.drawSize = graphic.drawSize = thingDef.graphic.drawSize = thingDef.graphicData.drawSize = new Vector2(thingDef.size.x, thingDef.size.z);
+                return graphic;
+            }
+        }
+        public override void Draw()
+        {
+            var oldDrawSize = def.graphicData.drawSize;
+            var thingDef = this.def.entityDefToBuild as ThingDef;
+            def.graphicData.drawSize = thingDef.graphicData.drawSize = new Vector2(thingDef.size.x, thingDef.size.z);
+            base.Draw();
+            def.graphicData.drawSize = thingDef.graphicData.drawSize = oldDrawSize;
+        }
+    }
+}
