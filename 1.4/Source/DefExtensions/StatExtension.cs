061b2d869acf467bb55198e0b1e709eac27a6a38
SargBjornson <sarg.auxiliar@gmail.com>
2023-08-15 20:02:30 +0200

    StatExtension for vehicles added

---
+++ 1.4/Source/DefExtensions/StatExtension.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using System.Collections.Generic;
+
+namespace VanillaVehiclesExpanded
+{
+
+    public class StatExtension : DefModExtension
+    {
+
+        public List<string> statToAdd = null;
+        public List<string> statValues = null;
+        public List<string> statDescriptions = null;
+        public bool showImageInInfoCard = false;
+        public string ImageToShowInInfoCard;
+
+    }
+
+}
