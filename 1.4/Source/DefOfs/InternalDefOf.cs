5c2f2120d214af7249f30affb33556bbbb102f98
SargBjornson <sarg.auxiliar@gmail.com>
2023-08-17 20:50:15 +0200

    Infocard image tweaks and Ideo stuff

---
+++ 1.4/Source/DefOfs/InternalDefOf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaVehiclesExpanded
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+       
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_SmallSpaces_Horrible;
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_Driving_Required;
+        [MayRequireIdeology]
+        public static PreceptDef VVE_Flying_Required;
+        [MayRequireIdeology]
+        public static PreceptDef VVE_Sailing_Required;
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_Acceleration_SundayDriver;
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_Acceleration_High;
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_VehicleRepairs_Slow;
+
+        [MayRequireIdeology]
+        public static PreceptDef VVE_VehicleRepairs_Fast;
+
+
+    }
+}
---
+++ 1.4/Source/PreceptComps/PreceptComp_DrivingRequired.cs
@@ -0,0 +1,34 @@
+﻿
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+namespace VanillaVehiclesExpanded
+{
+    public class PreceptComp_DrivingRequired : PreceptComp
+    {
+        public HistoryEventDef eventDef;
+
+       
+
+        public override void Notify_HistoryEvent(HistoryEvent ev, Precept precept)
+        {
+            if (ev.def != eventDef)
+            {
+                return;
+            }
+          
+            ev.args.TryGetArg(HistoryEventArgsNames.Doer, out Pawn pawn);
+           
+            if (pawn != null)
+            {
+                if (pawn.ideo?.Ideo?.HasPrecept(InternalDefOf.VVE_Driving_Required) == true)
+                {
+                    StaticCollectionsClass.AddOrSetColonistToLandVehicleList(pawn, 0);
+                 
+                }
+            }
+        }
+
+        
+    }
+}
