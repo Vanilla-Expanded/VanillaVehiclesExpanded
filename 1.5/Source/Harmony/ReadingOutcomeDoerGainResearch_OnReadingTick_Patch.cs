7f6f8aef2ebc5e9a41464844c27498e78666f9ea
Taranchuk <nfhfyxer@gmail.com>
2024-04-11 18:04:52 +0300

    patch fix

--- 1.5/Source/Harmony/ReadingOutcomeDoerGainResearch_OnReadingTick_Patch.cs
+++ 1.5/Source/Harmony/ReadingOutcomeDoerGainResearch_OnReadingTick_Patch.cs
@@ -6,7 +6,7 @@ namespace VanillaVehiclesExpanded
     [HarmonyPatch(typeof(ReadingOutcomeDoerGainResearch), "OnReadingTick")]
     public static class ReadingOutcomeDoerGainResearch_OnReadingTick_Patch
     {
-        public static void Prefix() => ResearchProjectDef_CanStartNow_Patch.shouldSkip = false;
+        public static void Prefix() => ResearchProjectDef_CanStartNow_Patch.shouldSkip = true;
 
         public static void Postfix() => ResearchProjectDef_CanStartNow_Patch.shouldSkip = false;
     }
